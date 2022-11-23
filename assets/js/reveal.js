3b37c1b11e402717611f6e06716376d415b01100
sonng147202 <sonnguyen20140720@gmail.com>
2022-11-23 16:07:07 +0700

    update

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -35,7 +35,67 @@ var iconCard = {
 //     origin: 'right',
 //     opacity: 0.5
 // }
+var discountChartImg = {
+    delay: 200,
+    distance: '50%',
+    duration: 1800,
+    cleanup: true,
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    },
+    reset: true,
+    origin: 'left',
+    opacity: 0
+};
+var sectionArrowDown = {
+    delay: 0,
+    distance: '100%',
+    duration: 1200,
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    },
+    reset: true,
+    origin: 'top',
+    opacity: 0
+};
+var s6WhyText = {
+    delay: 200,
+    distance: '100%',
+    duration: 1800,
+    cleanup: true,
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    },
+    reset: true,
+    origin: 'bottom',
+    opacity: 0
+};
+var arrowTop = {
+    delay: 1800,
+    distance: '150%',
+    duration: 1800,
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    },
+    origin: 'bottom',
+    opacity: 0
+};
 ScrollReveal().reveal('.img-workspace-la-gi', imgWorkspace);
 ScrollReveal().reveal('.s3-card-icon', iconCard);
 // ScrollReveal().reveal('.slick-slide', carouselItem);
+ScrollReveal().reveal('.discount-chart-img', discountChartImg);
+ScrollReveal().reveal('.section-arrow-down', sectionArrowDown);
+ScrollReveal().reveal('.s6-why-text', s6WhyText);
+
+ScrollReveal().reveal('.arrow-top', arrowTop);
+
+
 
